681e53c688b5affbbf30fe4322e7fbe396880e06
Ayham Darwish <ayham.darwish@sweco.se>
2024-09-05 08:30:02 +0200

    test push

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '^/api/SecureWebsite': {
+        target:'https://localhost:7247', // The URL of your .NET backend
+        secure: false, // Set to false if your backend uses a self-signed certificate
+      },
+    },
+  },
+});
